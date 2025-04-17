afe4ab4759b1921a3cb9db0f01f4d8924cde7917
Nikita277353 <lukancukila149@gmail.com>
2025-04-17 20:36:56 +0300

    Add files via upload

--- приложение здоровье, шаблон/final_win.py
+++ приложение здоровье, шаблон/final_win.py
@@ -12,3 +12,22 @@ from instr import*
 class FinalWin(QWidget):
     def __init__(self):
         super().__init__()
+        self.initUI()
+        self.set_appear()
+        self.show()
+
+
+    def initUI(self):
+        self.text_index = QLabel(txt_index)
+        self.text_workheart = QLabel(txt_workheart)
+
+
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.text_index, alignment= Qt.AlignCenter)
+        self.layout_line.addWidget(self.text_workheart, alignment= Qt.AlignCenter)
+        self.setLayout(self.layout_line)
+
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
